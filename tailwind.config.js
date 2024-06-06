3eb037e1f2e79fce4a7e6df0f0aaa7d22485a15f
JasonRavager <vergara.ecjr@gmail.com>
2024-06-06 10:28:07 +0800

    Add color splashes on hero banner.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       colors: {
         'primary': '#001D37',
         'secondary': '#475AFF',
+        'splash-1': '#D660FF',
+        'splash-2': '#6079FF',
+      },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, rgba(214,96,255,1) 0%, rgba(0,29,55,0.5) 60%, rgba(255,255,255,0) 100%)',
       },
     },
   },
