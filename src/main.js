1e9a7cc1a2e3c1e90f4f8a25b52e13e329325d52
JasonRavager <vergara.ecjr@gmail.com>
2024-06-07 14:05:56 +0800

    dynamic pet card

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+const template = document.getElementById("pet-card-template");
+const wrapper = document.createDocumentFragment();
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
   const weatherData = await weatherPromise.json();
@@ -11,9 +14,28 @@ start();
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
   const petsData = await petsPromise.json();
+
   petsData.forEach( pet => {
-    console.log(pet.name);
+    const clone = template.content.cloneNode(true);
+    clone.getElementById("pet-name").textContent = pet.name;
+    clone.getElementById("pet-description").textContent = pet.description;
+    clone.getElementById("pet-age").textContent = createAgeText(pet.birthYear);
+    clone.getElementById("pet-photo").src = pet.photo;
+    clone.getElementById("pet-photo").alt = `A ${pet.species} named ${pet.name}`;
+
+    wrapper.appendChild(clone);
   });
+  document.getElementById("list-of-pets").appendChild(wrapper);
 };
 
 petsArea();
+
+function createAgeText(birthYear) {
+  const year = new Date().getFullYear();
+  const age = year - birthYear;
+
+  if ( age == 0 ) return "less than a year old";
+  if ( age == 1 ) return "1 year old"
+  
+  return `${age} years old`;
+};
